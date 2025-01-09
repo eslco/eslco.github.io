b1823a4083a27e5492dba7dbe5a497a760fb2b74
eslco <59771891+eslco@users.noreply.github.com>
2025-01-09 07:07:34 -0800

    Site updated: 2025-01-09 07:07:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/09/Eslco-First-Post/","2025/01/09/hello-world/"];function toRandomPost(){
+var posts=["2025/01/09/hello-world/","2025/01/09/Eslco-First-Post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
